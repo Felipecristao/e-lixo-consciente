a8ad930a1190f5deb0322fadcbd7d55b5ef8f065
Felipecristao <96476036+Felipecristao@users.noreply.github.com>
2026-09-15 08:58:40 -0300

    Substitui camada de mapa que exigia chave

--- frontend/assets/js/mapa.js
+++ frontend/assets/js/mapa.js
@@ -58,13 +58,12 @@ function inicializarMapa() {
     );
 
     const camadaBasePrincipal = L.tileLayer(
-        "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
         {
             subdomains: "abcd",
-            maxZoom: 20,
+            maxZoom: 19,
             attribution:
-                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors ' +
-                '&copy; <a href="https://carto.com/attributions">CARTO</a>'
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }
     );
 
